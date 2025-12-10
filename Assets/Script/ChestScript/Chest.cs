7d95fdbd565bf1d981ce7b26cb880626b3fb0b6a
AlbayDev <119768008+Al-bai@users.noreply.github.com>
2025-12-10 14:45:15 +0700

    chest mechanics dan ui hotbar

--- Assets/Script/ChestScript/Chest.cs
+++ Assets/Script/ChestScript/Chest.cs
@@ -2,15 +2,67 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum ChestType
     {
-        
+        Crystal,
+        Tool
     }
 
-    // Update is called once per frame
+    public ChestType chestType;
+
+    private Animator anim;
+    private bool isOpen = false;
+    private bool playerNearby = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
-        
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleChest();
+        }
+    }
+
+    void ToggleChest()
+    {
+        isOpen = !isOpen;
+        anim.SetBool("isOpen", isOpen);
+
+        if (isOpen)
+        {
+            SpawnItem();
+        }
+    }
+
+    void SpawnItem()
+    {
+        if (chestType == ChestType.Crystal)
+        {
+            Debug.Log("Chest mengeluarkan CRYSTAL");
+        }
+        else if (chestType == ChestType.Tool)
+        {
+            Debug.Log("Chest mengeluarkan TOOL REPAIR");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+        }
     }
 }
